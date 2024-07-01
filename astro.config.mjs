a312822ae7d1a25906bf1e3c41c146d330b63dbc
Younes <bourhaneyounes1@gmail.com>
2024-07-01 05:10:58 +0100

    fixing paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+    base: "shibumi/",
     integrations: [tailwind()],
     output: 'server',
     adapter: vercel(),
